1ac323acfa49398cf1224bbf112925124691ef59
forgotten <forgotten>
2003-11-20 02:36:58 +0000

    tvmode.cpp replacement

--- src/scanline.cpp
+++ src/scanline.cpp
@@ -104,16 +104,124 @@ void Scanlines32(u8 *srcPtr, u32 srcPitch, u8 * /* deltaPtr */,
       *(nL) = 0;
       *(nL+1) = 0;
 
-      nextPixel = *bP++;
-      colorA = nextPixel;
-
       *(dP + 2) = colorB;
       *(dP + 3) = colorB;
       *(nL+2) = 0;      
       *(nL+3) = 0;
       
+      nextPixel = *bP++;
+
       dP += 4;
       nL += 4;
+    } while ((u8 *) bP < finish);
+    
+    srcPtr += srcPitch;
+    dstPtr += dstPitch*2;
+    nextLine += dstPitch*2;
+  }
+  while (--height);
+}
+
+void ScanlinesTV(u8 *srcPtr, u32 srcPitch, u8 * /* deltaPtr */,
+                 u8 *dstPtr, u32 dstPitch, int width, int height)
+{
+  u8 *nextLine, *finish;
+  u32 colorMask = ~(RGB_LOW_BITS_MASK | (RGB_LOW_BITS_MASK << 16));
+  
+  nextLine = dstPtr + dstPitch;
+  
+  do {
+    u32 *bP = (u32 *) srcPtr;
+    u32 *dP = (u32 *) dstPtr;
+    u32 *nL = (u32 *) nextLine;
+    u32 currentPixel;
+    u32 nextPixel;
+    
+    finish = (u8 *) bP + ((width+2) << 1);
+    nextPixel = *bP++;
+    
+    do {
+      currentPixel = nextPixel;
+      nextPixel = *bP++;
+      
+      u32 colorA, colorB;
+
+#ifdef WORDS_BIGENDIAN
+      colorA = (currentPixel & 0xFFFF0000) >> 16;
+      colorB = currentPixel & 0xFFFF;
+#else
+      colorA = currentPixel & 0xFFFF;
+      colorB = (currentPixel & 0xFFFF0000) >> 16;
+#endif
+      
+      *(dP) = colorA = colorA | ((((colorA & colorMask) >> 1) +
+                                  ((colorB & colorMask) >> 1))) << 16;
+      colorA = ((colorA & colorMask) >> 1);
+      colorA += ((colorA & colorMask) >> 1);
+      *(nL) = colorA;
+
+      colorA = nextPixel & 0xFFFF;
+
+      *(dP + 1) = colorB = colorB | ((((colorA & colorMask) >> 1) +
+                                      ((colorB & colorMask) >> 1))) << 16;
+      colorB = ((colorB & colorMask) >> 1);
+      colorB += ((colorB & colorMask) >> 1);
+
+      *(nL + 1) = colorB;      
+      
+      dP += 2;
+      nL += 2;
+    } while ((u8 *) bP < finish);
+    
+    srcPtr += srcPitch;
+    dstPtr += dstPitch*2;
+    nextLine += dstPitch*2;
+  }
+  while (--height);
+}
+
+#define RGB32_LOW_BITS_MASK 0x010101
+
+void ScanlinesTV32(u8 *srcPtr, u32 srcPitch, u8 * /* deltaPtr */,
+                   u8 *dstPtr, u32 dstPitch, int width, int height)
+{
+  u8 *nextLine, *finish;
+  u32 colorMask = ~RGB32_LOW_BITS_MASK;
+  
+  nextLine = dstPtr + dstPitch;
+  
+  do {
+    u32 *bP = (u32 *) srcPtr;
+    u32 *dP = (u32 *) dstPtr;
+    u32 *nL = (u32 *) nextLine;
+    u32 currentPixel;
+    u32 nextPixel;
+    
+    finish = (u8 *) bP + ((width+1) << 2);
+    nextPixel = *bP++;
+    
+    do {
+      currentPixel = nextPixel;
+      nextPixel = *bP++;
+      
+      u32 colorA, colorB, temp;
+
+      colorA = currentPixel;
+      colorB = nextPixel;
+      
+      *(dP) = colorA;
+      *(dP+1) = temp = ((colorA & colorMask) >> 1) +
+        ((colorB & colorMask) >> 1);
+      temp = ((temp & colorMask) >> 1);
+      temp += ((temp & colorMask) >> 1);
+      colorA = ((colorA & colorMask) >> 1);
+      colorA += ((colorA & colorMask) >> 1);
+
+      *(nL) = colorA;
+      *(nL+1) = temp;
+
+      dP += 2;
+      nL += 2;
     } while ((u8 *) bP < finish);
     
     srcPtr += srcPitch;
