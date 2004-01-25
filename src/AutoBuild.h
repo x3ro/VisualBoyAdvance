b4f173ea6a6e60415040d5ae70e3aab400bcd239
forgotten <forgotten>
2004-01-25 13:46:22 +0000

    Version update

--- src/AutoBuild.h
+++ src/AutoBuild.h
@@ -19,12 +19,12 @@
 #ifndef __AUTOBUILD_H__
 #define __AUTOBUILD_H__
 #ifndef VERSION
-#define VERSION       "1.7"
+#define VERSION       "1.7.1"
 #endif
 //change the FALSE to TRUE for autoincrement of build number
 #define INCREMENT_VERSION FALSE
-#define FILEVER        1,7,0,500
-#define PRODUCTVER     1,7,0,500
-#define STRFILEVER     "1, 7, 0, 500\0"
-#define STRPRODUCTVER  "1, 7, 0, 500\0"
+#define FILEVER        1,7,1,550
+#define PRODUCTVER     1,7,1,550
+#define STRFILEVER     "1, 7, 1, 550\0"
+#define STRPRODUCTVER  "1, 7, 1, 550\0"
 #endif //__AUTOBUILD_H__
